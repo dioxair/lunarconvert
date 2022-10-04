a5f7a6ceb188600f225292464ad05249b12eca7f
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-04 21:09:00 +0000

    Added file from select file button and video preview in app.js
    The video preview element was added in this commit fyi

--- public/app.js
+++ public/app.js
@@ -1,2 +1,4 @@
 const { createFFmpeg, fetchFile } = FFmpeg;
+const file = document.getElementById("fileUpload");
+const preview = document.getElementById("preview")
 const ffmpeg = createFFmpeg({ log: true });
