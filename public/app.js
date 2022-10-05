4be9f8067cbe82a2efccde2af7668d0996ad27b8
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-05 18:27:39 +0000

    Added functionality to download button

--- public/app.js
+++ public/app.js
@@ -2,6 +2,7 @@ const { createFFmpeg, fetchFile } = FFmpeg;
 const file = document.getElementById("fileUpload");
 const preview = document.getElementById("preview");
 const progress = document.getElementById("progress");
+const downloadButton = document.getElementById("downloadButton");
 const ffmpeg = createFFmpeg({ log: false });
 
 ffmpeg.setLogger(({ type, message }) => {
@@ -22,8 +23,12 @@ const transcode = async ({
 	ffmpeg.FS("writeFile", name, await fetchFile(files[0]));
 	await ffmpeg.run("-i", name, "output.mp4");
 	const data = ffmpeg.FS("readFile", "output.mp4");
-	preview.src = URL.createObjectURL(new Blob([data.buffer], {
+	const vidURL = URL.createObjectURL(new Blob([data.buffer], {
 		type: "video/mp4"
 	}));
+	console.log(vidURL);
+	preview.src = vidURL;
+	downloadButton.disabled = false;
+	downloadButton.onclick = function() { saveAs(vidURL, "output.mp4") };
 }
 file.addEventListener("change", transcode);
