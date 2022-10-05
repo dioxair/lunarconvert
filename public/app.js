70c94a6d4bf29f195a880c24d5816fe023158812
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-05 12:35:11 +0000

    Added functionality to FFmpeg conversion
    All you have to do is select a file and the conversion will automatically start. When finished, the video will display in the video preview element

--- public/app.js
+++ public/app.js
@@ -2,3 +2,21 @@ const { createFFmpeg, fetchFile } = FFmpeg;
 const file = document.getElementById("fileUpload");
 const preview = document.getElementById("preview")
 const ffmpeg = createFFmpeg({ log: true });
+
+const transcode = async ({
+	target: {
+		files
+	}
+}) => {
+	const {
+		name
+	} = files[0];
+	await ffmpeg.load();
+	ffmpeg.FS("writeFile", name, await fetchFile(files[0]));
+	await ffmpeg.run("-i", name, "output.mp4");
+	const data = ffmpeg.FS("readFile", "output.mp4");
+	preview.src = URL.createObjectURL(new Blob([data.buffer], {
+		type: "video/mp4"
+	}));
+}
+file.addEventListener("change", transcode);
