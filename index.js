eace08ffdb05c05e4a35c98c10473b65a30d54e0
Samuel Olagunju <94651716+samuelolagunju@users.noreply.github.com>
2022-10-08 00:33:04 +0000

    Installed and ran prettier

--- index.js
+++ index.js
@@ -3,59 +3,63 @@ const app = express();
 const fs = require("fs");
 
 app.use((_, res, next) => {
-	res.header("Cross-Origin-Opener-Policy", "same-origin");
-	res.header("Cross-Origin-Embedder-Policy", "require-corp");
-	next();
+  res.header("Cross-Origin-Opener-Policy", "same-origin");
+  res.header("Cross-Origin-Embedder-Policy", "require-corp");
+  next();
 });
 
 function getTime() {
-	const date = new Date();
+  const date = new Date();
 
-	let dateAndTime = date.toLocaleString('en-US', {
-		timeZone: 'America/New_York',
-	});
+  let dateAndTime = date.toLocaleString("en-US", {
+    timeZone: "America/New_York",
+  });
 
-	return dateAndTime;
+  return dateAndTime;
 }
 
 app.use(function logger(req, res, next) {
-	console.log(`${getTime()} EST - ${req.method} ${req.path} - ${req.ip}`);
-	console.log("")
-	fs.appendFile("logs.txt", `${getTime()} EST - ${req.method} ${req.path} - ${req.ip}
-`, err => {
-		if (err) {
-			console.log(err)
-		}
-	})
-	next();
+  console.log(`${getTime()} EST - ${req.method} ${req.path} - ${req.ip}`);
+  console.log("");
+  fs.appendFile(
+    "logs.txt",
+    `${getTime()} EST - ${req.method} ${req.path} - ${req.ip}
+`,
+    (err) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+  next();
 });
 
 app.get("/", (req, res) => {
-	res.sendFile(`${__dirname}/views/index.html`);
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
 app.get("/mp3", (req, res) => {
-	res.sendFile(`${__dirname}/views/mp3.html`);
+  res.sendFile(`${__dirname}/views/mp3.html`);
 });
 
 app.get("/avi", (req, res) => {
-	res.sendFile(`${__dirname}/views/avi.html`);
+  res.sendFile(`${__dirname}/views/avi.html`);
 });
 
 app.get("/flv", (req, res) => {
-	res.sendFile(`${__dirname}/views/flv.html`);
+  res.sendFile(`${__dirname}/views/flv.html`);
 });
 
 app.get("/gif", (req, res) => {
-	res.sendFile(`${__dirname}/views/gif.html`);
+  res.sendFile(`${__dirname}/views/gif.html`);
 });
 
 app.get("/mov", (req, res) => {
-	res.sendFile(`${__dirname}/views/mov.html`);
+  res.sendFile(`${__dirname}/views/mov.html`);
 });
 
 app.get("/mkv", (req, res) => {
-	res.sendFile(`${__dirname}/views/mkv.html`);
+  res.sendFile(`${__dirname}/views/mkv.html`);
 });
 
 app.use("/public", express.static(__dirname + "/public"));
@@ -63,5 +67,5 @@ app.use("/public", express.static(__dirname + "/public"));
 const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
-	console.log(`Server listening on port ${PORT}...`);
+  console.log(`Server listening on port ${PORT}...`);
 });
--- public/mov.js
+++ public/mov.js
@@ -9,61 +9,60 @@ let toggleButton = document.getElementById("toggleButton");
 let videoCategoryLabel = document.getElementById("videoCategoryLabel");
 let audioCategoryLabel = document.getElementById("audioCategoryLabel");
 function toggle() {
-	switch (formatGridDisplayVideo.style.display && formatGridDisplayAudio.style.display) {
-		case "none": {
-			formatGridDisplayVideo.style.display = "grid";
-			formatGridDisplayAudio.style.display = "grid";
-			videoCategoryLabel.style.display = "block";
-			audioCategoryLabel.style.display = "block";
-			toggleButton.textContent = "Hide formats";
-			break;
-		}
-		case "grid":
-			formatGridDisplayVideo.style.display = "none";
-			formatGridDisplayAudio.style.display = "none";
-			videoCategoryLabel.style.display = "none";
-			audioCategoryLabel.style.display = "none";
-			toggleButton.textContent = "Show formats";
-			break;
-		default:
-			formatGridDisplayVideo.style.display = "grid";
-			formatGridDisplayAudio.style.display = "grid";
-			videoCategoryLabel.style.display = "block";
-			audioCategoryLabel.style.display = "block";
-			toggleButton.textContent = "Hide formats";
-			break;
-	}
+  switch (
+    formatGridDisplayVideo.style.display &&
+    formatGridDisplayAudio.style.display
+  ) {
+    case "none": {
+      formatGridDisplayVideo.style.display = "grid";
+      formatGridDisplayAudio.style.display = "grid";
+      videoCategoryLabel.style.display = "block";
+      audioCategoryLabel.style.display = "block";
+      toggleButton.textContent = "Hide formats";
+      break;
+    }
+    case "grid":
+      formatGridDisplayVideo.style.display = "none";
+      formatGridDisplayAudio.style.display = "none";
+      videoCategoryLabel.style.display = "none";
+      audioCategoryLabel.style.display = "none";
+      toggleButton.textContent = "Show formats";
+      break;
+    default:
+      formatGridDisplayVideo.style.display = "grid";
+      formatGridDisplayAudio.style.display = "grid";
+      videoCategoryLabel.style.display = "block";
+      audioCategoryLabel.style.display = "block";
+      toggleButton.textContent = "Hide formats";
+      break;
+  }
 }
 ffmpeg.setLogger(({ type, message }) => {
-	progress.style.display = "block";
-	progress.style.color = "khaki";
+  progress.style.display = "block";
+  progress.style.color = "khaki";
   progress.textContent = `OUTPUT: ${message}`;
 });
 
-const transcode = async ({
-	target: {
-		files
-	}
-}) => {
-	const {
-		name
-	} = files[0];
-	if (ffmpeg.isLoaded() === false) {
-		await ffmpeg.load();
-	}
-	ffmpeg.FS("writeFile", name, await fetchFile(files[0]));
-	await ffmpeg.run("-i", name, "output.mov");
-	const data = ffmpeg.FS("readFile", "output.mov");
-	const vidURL = URL.createObjectURL(new Blob([data.buffer], {
-		type: "video/quicktime"
-	}));
-	console.log(vidURL);
-	downloadButton.disabled = false;
-	downloadButton.onclick = function() {
+const transcode = async ({ target: { files } }) => {
+  const { name } = files[0];
+  if (ffmpeg.isLoaded() === false) {
+    await ffmpeg.load();
+  }
+  ffmpeg.FS("writeFile", name, await fetchFile(files[0]));
+  await ffmpeg.run("-i", name, "output.mov");
+  const data = ffmpeg.FS("readFile", "output.mov");
+  const vidURL = URL.createObjectURL(
+    new Blob([data.buffer], {
+      type: "video/quicktime",
+    })
+  );
+  console.log(vidURL);
+  downloadButton.disabled = false;
+  downloadButton.onclick = function () {
     let str = String(vidURL);
-    str = str.slice(42) // get's the blob name
-    saveAs(vidURL, `${str}.mov`)
-	};
-	progress.textContent = "Conversion is done!";
-}
+    str = str.slice(42); // get's the blob name
+    saveAs(vidURL, `${str}.mov`);
+  };
+  progress.textContent = "Conversion is done!";
+};
 file.addEventListener("change", transcode);
